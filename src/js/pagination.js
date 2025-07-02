4e70ac80bae3a3c0bc0cd8eb65bfce9c7778a44a
totkda <jdnfurgobe34@gmail.com>
2025-07-02 10:21:01 +0900

    order_manageとdelivery_manageをまとめてpagination.jsに変更

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,4 +1,4 @@
-// desktop\code\index.js
+// ページネーション（ページ送り）機能を実装するJavaScriptです。
 
 document.addEventListener('DOMContentLoaded', function () {
     const totalPages = 20;    // 総ページ数（好きな数字でOK）
@@ -26,12 +26,13 @@ document.addEventListener('DOMContentLoaded', function () {
         let pages = [];
 
         if (totalPages <= 10) {
-            // 全部表示
+            // ページ数が10以下なら全ページ番号を表示
             for (let i = 1; i <= totalPages; i++) {
                 pages.push(i);
             }
         } else {
-            // たくさんある場合
+            // 11以上なら「1 ... 前後ページ ... 最終ページ」と省略表示
+            //現在のページはボタンがアクティブ表示
             pages.push(1); // 1ページ目は必ず
             let start = Math.max(2, currentPage - 1);
             let end = Math.min(totalPages - 1, currentPage + 1);
