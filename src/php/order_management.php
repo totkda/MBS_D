09c00641a360d734fd924a840efee65bc3399779
totkda <jdnfurgobe34@gmail.com>
2025-07-14 11:10:15 +0900

    JSの読み込みパスを修正

--- src/php/order_management.php
+++ src/php/order_management.php
@@ -170,8 +170,7 @@ try {
             </div>
         </div>
     </main>
-    <script src="./js/pagination.js"></script>
-    <!-- 必要なJSの読み込み -->
+    <script src="/src/js/pagination.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
