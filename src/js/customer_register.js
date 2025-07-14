53b2fc2af7bb769aac21abbd34715efca5ddcfdb
totkda <jdnfurgobe34@gmail.com>
2025-07-14 12:10:55 +0900

    顧客取込処理のフォーム送信時に完了モーダルを表示する機能を追加し、CSVファイルのアップロード時にファイル形式をチェックするように変更。また、モーダルの内容を更新しました。

--- src/js/customer_register.js
+++ src/js/customer_register.js
@@ -1,9 +1,13 @@
 // ドキュメントの読み込みが完了したら実行
 $(function () {
-    // ボタンがクリックされたときの処理
-    $('#client-insert-button').on('click', function () {  // #①ポップアップを出すボタンにid属性を追加し、ボタンの名前をつける ②#ボタンの名前 に変更する
-        // モーダルウィンドウを取得し、表示する
-        const modal = new bootstrap.Modal(document.getElementById('client-insert'));  //  上で付けたポップアップの名前に変更する
-        modal.show();
+    // フォーム送信時に完了モーダルを表示し、送信を一時停止
+    $('#import-form').on('submit', function (e) {
+        e.preventDefault();
+        const completeModal = new bootstrap.Modal(document.getElementById('import-complete'));
+        completeModal.show();
+        // モーダルの「閉じる」ボタンでフォームを実際に送信
+        $('#import-complete-close').one('click', () => {
+            e.target.submit();
+        });
     });
 });
